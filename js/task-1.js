e2945bc74d44490588bf2ed8537360dc61e13cae
Yurin <yuriinnx@gmail.com>
2025-10-30 19:12:57 +0200

    do task 1 js

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,10 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const allLi = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${allLi.length}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+allLi.forEach(element => {
+    const h2 = element.querySelector("h2");
+    const allLiOfLi = element.querySelectorAll("li");
+    console.log(`Category: ${h2.textContent}`);
+    console.log(`Elements: ${allLiOfLi.length}`);
+});
+
