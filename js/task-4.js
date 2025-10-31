f3ab332bf7fcbfc3cf53f00635eed8ba992c911c
Yurin <yuriinnx@gmail.com>
2025-10-31 22:35:24 +0200

    do task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+
+    if (email === "" || password === "") {
+        alert('All form fields must be filled in');
+    } else {
+        const object = {
+            email,
+            password,
+        };
+        console.log(object);
+        form.reset();
+    }
+});
