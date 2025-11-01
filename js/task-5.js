0544ef66fb10da998afe979842d64c7f9e4893cb
Yurin <yuriinnx@gmail.com>
2025-11-01 23:53:04 +0200

    do task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector("button.change-color");
+const span = document.querySelector("span.color");
+
+button.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+})
